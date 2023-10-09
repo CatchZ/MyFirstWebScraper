980959e57e9d207072d6a60430b42ff287f5e08c
CatchZ <maximilian.ziemann@gmx.de>
2023-10-09 21:06:16 +0200

    added connection to target website

--- simple-web-scraper-java/src/main/java/Main.java
+++ simple-web-scraper-java/src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        // initializing the HTML Document page variable
+        Document doc;
+
+        try {
+            // fetching the target website
+            doc = Jsoup.connect("https://scrapeme.live/shop").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
