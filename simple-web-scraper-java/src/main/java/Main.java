d105617e05d0a087de3e3abb40837554ea0950e4
CatchZ <maximilian.ziemann@gmx.de>
2023-10-12 22:01:51 +0200

    added Podcast Link List changed methods for more reusability

--- simple-web-scraper-java/src/main/java/Main.java
+++ simple-web-scraper-java/src/main/java/Main.java
@@ -1,20 +1,20 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import  simple_web_scraper.Simple_Web_Scraper;
 import spiegel_Podacast_Scraper.*;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
+     final String SPIEGEL_AUDIO_URL = "https://www.spiegel.de/audio/";
     //   Simple_Web_Scraper.web_Scraper();
-        SpiegelPodcastScrapper.spiegelPodcastScrapper();
+       System.out.println( SpiegelPodcastScrapper.spiegelPodcastSelector(
+               SpiegelPodcastScrapper.getDocumentOfURl(SPIEGEL_AUDIO_URL)
+       ));
+    System.out.println(
+            SpiegelPodcastScrapper.generateSpiegelPodcastUrlList(
+                    SpiegelPodcastScrapper.spiegelPodcastSelector(
+                            SpiegelPodcastScrapper.getDocumentOfURl(SPIEGEL_AUDIO_URL)
+                    )
+            )
+    );
+
 
     }
 
--- simple-web-scraper-java/src/main/java/spiegel_Podacast_Scraper/SpiegelPodcastScrapper.java
+++ simple-web-scraper-java/src/main/java/spiegel_Podacast_Scraper/SpiegelPodcastScrapper.java
@@ -7,35 +7,41 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.LinkedList;
 
 public class SpiegelPodcastScrapper {
-    private static final String URL = "https://www.spiegel.de/audio/";
+
     private static final String PODCASTTITELSELECTOR = "a.group.block.text-black";
 
 
 
-    public static void spiegelPodcastScrapper() {
-        Document doc;
-
+    public static Document getDocumentOfURl(String url) {
+     Document document ;
         try {
             // fetching the target website
-            doc = Jsoup
-                    .connect(URL)
+            document = Jsoup
+                    .connect(url)
                     .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36")
                     .get();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        Elements h1 = doc.select( PODCASTTITELSELECTOR);
-
-        System.out.println("result:");
-        System.out.println(h1.toString());
+        return document;
     }
+
+    public static Elements spiegelPodcastSelector(Document document){
+        return document.select( PODCASTTITELSELECTOR);
+    }
+
+    public static LinkedList<String> generateSpiegelPodcastUrlList(Elements podcastElements){
+        LinkedList <String> resultList = new LinkedList<>();
+        for(Element podcastElement : podcastElements){
+            resultList.add(podcastElement.attr("href"));
+        }
+        resultList.forEach(e->System.out.println(e));
+        return resultList;
+    }
+
 
 
 }
