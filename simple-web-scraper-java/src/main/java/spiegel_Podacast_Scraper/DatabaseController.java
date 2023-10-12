081430c3db4e5f79a7797ba5c554ed175e589873
CatchZ <maximilian.ziemann@gmx.de>
2023-10-12 23:48:12 +0200

    First Line Code for Database Controler

--- simple-web-scraper-java/src/main/java/spiegel_Podacast_Scraper/DatabaseController.java
+++ simple-web-scraper-java/src/main/java/spiegel_Podacast_Scraper/DatabaseController.java
@@ -1,4 +1,11 @@
 package spiegel_Podacast_Scraper;
 
 public class DatabaseController {
+    private final String URL = "jdbc:postgresql://localhost/myDB";
+    private final String USER = "postgres";
+    private final String PASSWORD = "root";
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO users" +
+            "  (episode, Podcast) VALUES " +
+            " (?, ?);";
 }
